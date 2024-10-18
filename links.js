2ebb49278ead4fdb55f185fe1fef67a52f9d94e1
Maksim Drobyshev <maks88sgt@yandex.ru>
2024-10-18 19:21:45 +0300

    18.10

---
+++ links.js
@@ -0,0 +1,6 @@
+const links=[
+    "https://static.tildacdn.com/tild3932-3637-4233-b235-613362306535/4.jpg",
+    "https://pic.rutubelist.ru/video/75/62/7562385dd0269bd26e08489efc3f0fd2.jpg",
+    "https://www.multitour.ru/files/excursimgs/excurs_56909_201749_07.jpg",
+    "https://i.pinimg.com/originals/94/d6/51/94d651d70c3702cb5a488365867d71d0.jpg"
+]
