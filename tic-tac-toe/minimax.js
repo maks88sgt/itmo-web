938e7d4553d1e857d8f3fa15087b93d145b02241
Maksim Drobyshev <maks88sgt@yandex.ru>
2024-11-06 18:39:32 +0300

    tic-tac-toe initial commit

---
+++ tic-tac-toe/minimax.js
@@ -0,0 +1,47 @@
+function minimax(newBoard, player) {
+    const availableSpots = newBoard.reduce((acc, val, idx) => (val === '' ? acc.concat(idx) : acc), []);
+
+    if (checkWin(newBoard, 'X')) return { score: -10 };
+    if (checkWin(newBoard, 'O')) return { score: 10 };
+    if (availableSpots.length === 0) return { score: 0 };
+
+    const moves = [];
+    for (let i = 0; i < availableSpots.length; i++) {
+        const index = availableSpots[i];
+        const move = { index };
+
+        newBoard[index] = player;
+
+        if (player === 'O') {
+            const result = minimax(newBoard, 'X');
+            move.score = result.score;
+        } else {
+            const result = minimax(newBoard, 'O');
+            move.score = result.score;
+        }
+
+        newBoard[index] = '';
+        moves.push(move);
+    }
+
+    let bestMove;
+    if (player === 'O') {
+        let bestScore = -Infinity;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = moves[i];
+            }
+        }
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = moves[i];
+            }
+        }
+    }
+
+    return bestMove;
+}
