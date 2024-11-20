a8b391bf5d035dfdba6b071bfa493c8b78513fd0
Maksim Drobyshev <maks88sgt@yandex.ru>
2024-11-20 19:01:16 +0300

    fix form bug

--- form/Form.js
+++ form/Form.js
@@ -1,72 +1,85 @@
-export class Form{
-    static formCounter = 0
+export class Form {
+  static formCounter = 0;
 
-    constructor(fields = [], onSubmit = null){
-        this.fields = fields
-        this.onSubmit = onSubmit
-        this.id = `Form-${Form.formCounter}`
-        Form.formCounter += 1
+  constructor(fields = [], onSubmit = null) {
+    this.fields = fields;
+    this.onSubmit = onSubmit;
+    this.id = `Form-${Form.formCounter}`;
+    Form.formCounter += 1;
+  }
+
+  render() {
+    const form = document.createElement("form");
+    form.id = this.id;
+    const fragment = document.createDocumentFragment();
+    for (const field of this.fields) {
+      fragment.appendChild(field.render());
     }
+    const button = document.createElement("button");
+    button.type = "submit";
+    button.innerText = "Submit";
+    fragment.appendChild(button);
+    form.appendChild(fragment);
+    return form;
+  }
 
-    render(){
-        const form = document.createElement('form')
-        form.id = this.id
-        const fragment = document.createDocumentFragment()
-            for (const field of this.fields){
-                fragment.appendChild(field.render())
-            }
-        const button = document.createElement('button')
-        button.type = 'submit'
-        button.innerText="Submit"
-        fragment.appendChild(button)
-        form.appendChild(fragment)
-        return form   
+  getData() {
+    const data = {};
+    this.fields.forEach((field) => {
+      data[field.name] = field.value;
+    });
+    return data;
+  }
+
+  validate() {
+    return this.fields.every((field) => field.validate());
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (this.validate()) {
+      const data = this.getData();
+      this.onSubmit(data);
+    } else {
+      console.log(this.fields);
+      this.updateView();
     }
+  }
 
-    getData(){
-        const data = {}
-        this.fields.forEach(field=>{
-            data[field.name] = field.value
-        })
-        return data
-    }
+  // updateView() {
+  //   const formContainer = document.querySelector(`#${this.id}`).parentElement;
+  //   formContainer.innerHTML = "";
+  //   formContainer.appendChild(this.render());
+  //   this.attachListeners();
+  // }
 
-    validate(){
-        return this.fields.every(field=>field.validate())
-    }
+  updateView() {
+    this.fields.forEach((field) => {
+      const inputElement = document.querySelector(`#${field.name}`);
+      const errorElement = inputElement.nextElementSibling; // Элемент `<span>` с текстом ошибки
 
-    handleSubmit(event){
-        event.preventDefault()
-        if(this.validate()){
-            const data = this.getData()
-            this.onSubmit(data)
-        } else {
-            console.log(this.fields)
-            this.updateView()
-        }
-        
-    }
+      // Обновляем значение (не обязательно, если уже меняется через `input`)
+      inputElement.value = field.value;
 
+      // Обновляем текст ошибки
+      if (errorElement) {
+        errorElement.textContent = field.error;
+      }
+    });
+   }
 
-    updateView(){
-        const formContainer = document.querySelector(`#${this.id}`).parentElement
-        formContainer.innerHTML=""
-        formContainer.appendChild(this.render())
-        //this.attachListeners()
-    }
-
-    attachListeners(){
-        const form = document.querySelector(`#${this.id}`)
-        form.addEventListener("submit", this.handleSubmit.bind(this))
-        this.fields.forEach(field=>{
-            const inputElement = document.querySelector(`#${field.name}`)
-            inputElement.addEventListener("input", event=>{
-                field.value = event.target.value;
-                this.updateView()
-            })
-        })
-    }
-
+  attachListeners() {
+    const form = document.querySelector(`#${this.id}`);
+    form.addEventListener("submit", this.handleSubmit.bind(this));
+    this.fields.forEach((field) => {
+      const inputElement = document.querySelector(`#${field.name}`);
+      inputElement.addEventListener("input", (event) => {
+        field.value = event.target.value;
+        field.error = "";
+        this.updateView();
+      });
+    });
+  }
 }
 
-export default Form
+export default Form;
