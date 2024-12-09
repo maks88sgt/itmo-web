326b47f3779b8c5321259ee37e4263b0b6914177
Maksim Drobyshev <maks88sgt@yandex.ru>
2024-12-09 18:35:28 +0300

    basic components

--- react-app/src/components/asteroid-card/AsteroidCard.tsx
+++ react-app/src/components/asteroid-card/AsteroidCard.tsx
@@ -1,26 +1,45 @@
-import { AsteroidCardButton } from "./AsteroidCardButton"
-import { AsteroidCardData } from "./AsteroidCardData"
-import { AsteroidCardImage } from "./AsteroidCardImage"
-import style from "./card.module.scss"
+import { Dispatch, SetStateAction } from "react";
+import { AsteroidCardButton } from "./AsteroidCardButton";
+import { AsteroidCardData } from "./AsteroidCardData";
+import { AsteroidCardImage } from "./AsteroidCardImage";
+import style from "./card.module.scss";
 
-
-interface AsteroidCardProps {
-    isDangerous: boolean
-    name: string
-    distanceKm: number
-    distanceMoon: number
-    date: string
-    radius: number
-    isKilometers: boolean
+export interface AsteroidCardProps {
+  isDangerous: boolean;
+  name: string;
+  distanceKm: number;
+  distanceMoon: number;
+  date: string;
+  radius: number;
+  isKilometers: boolean;
 }
 
-export const AsteroidCard = (props: AsteroidCardProps)=>{
+export const AsteroidCard = (
+  props: AsteroidCardProps & {
+    setDestroyment: Dispatch<AsteroidCardProps[]>;
+  }
+) => {
+  const { isDangerous, setDestroyment } = props;
 
-    const {isDangerous} = props
-
-    return <div className={`${style.card} ${isDangerous ? style.dangerous : ""}`}>
-        <AsteroidCardImage/>
-        <AsteroidCardData/>
-        <AsteroidCardButton/>
+  return (
+    <div className={`${style.card} ${isDangerous ? style.dangerous : ""}`}>
+      <AsteroidCardImage />
+      <AsteroidCardData {...props} />
+      <AsteroidCardButton
+        onClick={() =>
+          setDestroyment(
+            //@ts-ignore
+            (destroyment: AsteroidCardProps[]) => {
+              if (destroyment.some((it) => it.name === props.name)) {
+                return destroyment.filter((it) => it.name === props.name);
+              } else {
+                destroyment.push(props);
+                return destroyment
+              }
+            }
+          )
+        }
+      />
     </div>
-}
+  );
+};
