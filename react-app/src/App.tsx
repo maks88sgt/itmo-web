7a19cb23e0e8dad19866ca2c5ea3be76469f3be4
Maksim Drobyshev <maks88sgt@yandex.ru>
2024-12-22 12:38:55 +0300

    add mui

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,32 +1,39 @@
 import styles from "./App.module.css";
 import { AppHeader } from "./components/app-header/AppHeader";
-import {
-  BrowserRouter,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Asteroid } from "./components/Asteroid";
 import { Asteroids } from "./components/Asteroids";
 import { Destroyment } from "./components/Destroyment";
 import StoreProvider from "./store/StoreProvider";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { orange } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#7de8ff",
+    },
+  },
+});
 
 export const App = () => {
   return (
     <StoreProvider>
-      <div className={styles.app}>
-        <BrowserRouter>
-          <AppHeader />
-          <Routes>
-            <Route path="/" element={<Navigate to="/asteroids" />} />
-            <Route path="/asteroids" element={<Asteroids />} />
-            <Route path="/destroyment" element={<Destroyment />} />
-            <Route path="/asteroids/:id" element={<Asteroid />} />
-            <Route path="*" element={<Navigate to="/asteroids" />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-      </StoreProvider>
+      <ThemeProvider theme={theme}>
+        <div className={styles.app}>
+          <BrowserRouter>
+            <AppHeader />
+            <Routes>
+              <Route path="/" element={<Navigate to="/asteroids" />} />
+              <Route path="/asteroids" element={<Asteroids />} />
+              <Route path="/destroyment" element={<Destroyment />} />
+              <Route path="/asteroids/:id" element={<Asteroid />} />
+              <Route path="*" element={<Navigate to="/asteroids" />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </StoreProvider>
   );
 };
 
--- react-app/src/store/useSelector.tsx
+++ react-app/src/store/useSelector.tsx
@@ -38,8 +38,8 @@ function shallowEqual(obj1: any, obj2: any) {
   }
 
   const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
+  const keys2 = Object.keys(obj2); 
+ 
   if (keys1.length !== keys2.length) return false;
 
   for (const key of keys1) {
